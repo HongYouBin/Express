5df0ed2c411988fc3d7ba8c68ddd563198c44774
HongYouBin <ghddbqls33@gmail.com>
2022-06-24 01:39:37 +0900

    catModel, middleware added

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var appModel_1 = require("./appModel");
 var app = express();
 var port = 8000;
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is middle");
+    next();
+});
 app.get("/", function (req, res) {
     console.log(req);
-    res.send("Hello World!!!!!!!!");
+    res.send({ cats: appModel_1.Cat });
+});
+app.get("/cats/blue", function (req, res, next) {
+    res.send({ blue: appModel_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: appModel_1.Cat[1] });
+});
+app.use(function (req, res, next) {
+    console.log("error");
+    res.send({ error: "404 not found err" });
 });
 app.listen(port, function () {
-    console.log("Example app listening at http://localhost:" + port + "/");
+    console.log("server is on... http://localhost:" + port + "/");
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,36 @@
 import * as express from "express";
+import { Cat, CatType } from "./appModel";
+
 const app: express.Express = express(); //app -> express의 instance
 
 const port: number = 8000;
 
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is middle");
+  next();
+});
+
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log(req);
-  res.send("Hello World!!!!!!!!");
+  res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log("error");
+  res.send({ error: "404 not found err" });
+  //next();
 });
 
 app.listen(port, () => {
   //서버를 연다.
-  console.log(`Example app listening at http://localhost:${port}/`);
+  console.log(`server is on... http://localhost:${port}/`);
 });
