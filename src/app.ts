2cf514ec3b7af19535cf2e4799f3c8eabf6c6bc5
HongYouBin <ghddbqls33@gmail.com>
2022-06-24 03:30:44 +0900

    singleton pattern & service pattern

--- src/app.ts
+++ src/app.ts
@@ -6,26 +6,57 @@ const app: express.Express = express(); //app -> express의 instance
 
 const port: number = 8000;
 
-//logging middelware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("this is middle");
-  next();
-});
+class Server {
+  public app: express.Application;
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
-//JSON middleware
-app.use(express.json());
+  private setRoute() {
+    //route middleware
+    this.app.use(catsRouter);
+  }
 
-app.use(catsRouter);
+  private setMiddleware() {
+    //logging middelware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("this is middle");
+      next();
+    });
 
-//404 middleware
-app.use((req, res, next) => {
-  console.log("error");
-  res.send({ error: "404 not found err" });
-  //next();
-});
+    //JSON middleware
+    this.app.use(express.json());
 
+    this.setRoute();
+
+    //404 middleware
+    this.app.use((req, res, next) => {
+      console.log("error");
+      res.send({ error: "404 not found err" });
+      //next();
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log("server is on ...");
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
+
+/*
 app.listen(port, () => {
   //서버를 연다.
   console.log(`server is on... http://localhost:${port}/`);
 });
+*/
